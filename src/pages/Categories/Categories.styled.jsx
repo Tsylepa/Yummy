e5c45b4fd49b8f99fde5fe438aa4c527edae0ae6
KutsaevPavlo <p1ul@ukr.net>
2023-07-17 23:14:56 +0300

    update categories

--- src/pages/Categories/Categories.styled.jsx
+++ src/pages/Categories/Categories.styled.jsx
@@ -37,7 +37,7 @@ export const StyledCategoriesPagesWraper = styled.div`
 
   @media screen and (min-width: 1440px) {
     width: 1440px;
-    padding-left: 100px;
-    padding-right: 100px;
+    // padding-left: 100px;
+    // padding-right: 100px;
   }
 `;
