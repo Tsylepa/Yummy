4fc6baedc4924f0b4cecc0e1a4b98d93e925771d
tsylepa <tsylepa@gmail.com>
2023-07-20 23:54:46 +0100

    Cards styles

--- src/components/RecipesCard/RecipesCard.jsx
+++ src/components/RecipesCard/RecipesCard.jsx
@@ -1,26 +1,27 @@
 import {
   StyledCards,
-  StyledCardsList,
+  Card,
   StyledCardsWrapper,
   StyledCardsImg,
   StyledCardsTextWrapper,
   StyledCardsText,
 } from './RecipesCard.styles';
+import { Link } from 'react-router-dom';
 
 export const RecipesCard = ({ recipes }) => {
   return (
     <StyledCards>
       {recipes.map(({ _id, title, preview }) => (
-        <li key={_id}>
-          <StyledCardsList to={`/recipe/${_id}`}>
+        <Card key={_id}>
+          <Link to={`/recipe/${_id}`}>
             <StyledCardsWrapper>
               <StyledCardsImg src={preview} alt={title} />
               <StyledCardsTextWrapper>
                 <StyledCardsText>{title}</StyledCardsText>
               </StyledCardsTextWrapper>
             </StyledCardsWrapper>
-          </StyledCardsList>
-        </li>
+          </Link>
+        </Card>
       ))}
     </StyledCards>
   );
