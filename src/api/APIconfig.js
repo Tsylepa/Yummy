26f549d63fd0c5ec207917971687e3761c54143a
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-07-21 13:15:22 +0300

    fix refresh token

--- src/api/APIconfig.js
+++ src/api/APIconfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'https://soyummy-backend-kmc6.onrender.com/api',
 });
 
 instance.interceptors.response.use(
